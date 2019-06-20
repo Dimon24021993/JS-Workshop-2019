40c9220d50bee780d0c868d8faac69eb458c855d
Дмитрий Хмельницкий <khmelnytskiy93@rambler.ru>
2019-06-20 19:50:49 +0300

    day_2

--- script.js
+++ script.js
@@ -4,26 +4,30 @@ const apiKey = '67052673bb356cf1a6e9736d875f4551';
 const imagePath = 'https://image.tmdb.org/t/p/w500';
 
 function apiSearch(e) {
+    e.preventDefault();
     const searchText = document.querySelector('#search-text').value,
         url = `https://api.themoviedb.org/3/search/multi?api_key=${apiKey}&language=ru-Ru&query=${searchText}&include_adult=true`
-    requestApi("GET", url);
-    e.preventDefault();
+
+    fetch(url)
+        .then(function (value) {
+            if (value.status !== 200) {
+                return Promise.reject(value);
+            }
+            return value.json();
+        })
+        .then(function (output) {
+                movie.innerHTML = output.results.map(m => {
+                    return '<div class="col-12 col-md-6 col-lg-4 col-xl-3 item">' +
+                        (m.poster_path ? `<img src='${imagePath+m.poster_path}' class="img"/>` : "<div class='img'></div>") +
+                        `<span>${m.name||m.title}</span>
+            </div>`;
+                }).join('');
+            },
+            function (reason) {
+                movie.innerHTML = 'Oops, some-thing goes wrong!';
+                console.log('error: ' + reason.status);
+            }).catch(er => {
+            console.warn(`error in render block`);
+        });
 }
 searchForm.addEventListener('submit', apiSearch);
-
-function requestApi(method, url, data) {
-    var request = new XMLHttpRequest();
-    request.open(method, url);
-    request.addEventListener('readystatechange', r => {
-        if (request.readyState !== 4 || request.status !== 200) return;
-        const output = JSON.parse(request.responseText);
-        console.log(output)
-        movie.innerHTML = output.results.map(m => {
-            return `<div class="col-12 col-md-6 col-lg-4 col-xl-3">
-            <img src='${imagePath+m.poster_path}'/>
-            <span>${m.name||m.title}</span>
-            </div>`;
-        }).join('');
-    });
-    request.send(data);
-}
